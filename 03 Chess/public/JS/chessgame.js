ab8e3a4b272da590fb04d814e7be8703f0c81fa5
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-05-12 17:38:33 +0530

    chessgame completed

--- 03 Chess/public/JS/chessgame.js
+++ 03 Chess/public/JS/chessgame.js
@@ -28,15 +28,98 @@ const renderBoard = () => {
           square.color === "w" ? "white" : "black",
         );
 
-        pieceElement.innerText = "";
+        pieceElement.innerText = getPieceUnicode(square);
         pieceElement.draggable = playerRole === square.color;
 
-        pieceElement.addEventListener("dragstart" , () => {
-            
-        })
+        pieceElement.addEventListener("dragstart", (e) => {
+          if (pieceElement.draggable) {
+            draggedPiece = pieceElement;
+            sourceSquare = { row: rowIndex, col: squareIndex };
+            e.dataTransfer.setData("text/plain", "");
+          }
+        });
+        pieceElement.addEventListener("dragend", (e) => {
+          draggedPiece = null;
+          sourceSquare = null;
+        });
+
+        squareElement.appendChild(pieceElement);
       }
+
+      squareElement.addEventListener("dragover", (e) => {
+        e.preventDefault();
+      });
+
+      squareElement.addEventListener("drop", (e) => {
+        e.preventDefault();
+        if (draggedPiece) {
+          const targetSource = {
+            row: parseInt(squareElement.dataset.row),
+            col: parseInt(squareElement.dataset.col),
+          };
+
+          handleMove(sourceSquare, targetSource);
+        }
+      });
+      boardElemet.append(squareElement);
     });
   });
+
+  if (playerRole === "b") {
+    boardElemet.classList.add("flipped");
+  } else {
+    boardElemet.classList.remove("flipped");
+  }
 };
+
+const handleMove = (source, target) => {
+  const move = {
+    from: `${String.fromCharCode(97 + source.col)}${8 - source.row}`,
+    to: `${String.fromCharCode(97 + target.col)}${8 - target.row}`,
+    promotion: "q",
+  };
+
+  socket.emit("move", move);
+};
+
+const getPieceUnicode = (piece) => {
+  const unicodePieces = {
+    wp: "♙",
+    wr: "♖",
+    wn: "♘",
+    wb: "♗",
+    wq: "♕",
+    wk: "♔",
+
+    bp: "♟",
+    br: "♜",
+    bn: "♞",
+    bb: "♝",
+    bq: "♛",
+    bk: "♚",
+  };
+
+  return unicodePieces[piece.color + piece.type];
+};
+
+socket.on("playerRole", (role) => {
+  playerRole = role;
+  renderBoard();
+});
+
+socket.on("spectatorRole", () => {
+  playerRole = null;
+  renderBoard();
+});
+
+socket.on("boardState", (fen) => {
+  chess.load(fen);
+  renderBoard();
+});
+
+socket.on("move", (move) => {
+  chess.move(move);
+  renderBoard();
+});
 
 renderBoard();

4998680bf8ac28255c81c6bce9265305e3995729
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-05-12 16:34:35 +0530

    chess functionality

--- 03 Chess/public/JS/chessgame.js
+++ 03 Chess/public/JS/chessgame.js
@@ -1,5 +1,3 @@
-const { Chess } = require("chess.js");
-
 const socket = io();
 const chess = new Chess();
 
@@ -8,3 +6,37 @@ const boardElemet = document.querySelector(".chessboard");
 let draggedPiece = null;
 let sourceSquare = null;
 let playerRole = null;
+
+const renderBoard = () => {
+  const board = chess.board();
+  boardElemet.innerHTML = "";
+  board.forEach((row, rowIndex) => {
+    row.forEach((square, squareIndex) => {
+      const squareElement = document.createElement("div");
+      squareElement.classList.add(
+        "square",
+        (rowIndex + squareIndex) % 2 === 0 ? "light" : "dark",
+      );
+
+      squareElement.dataset.row = rowIndex;
+      squareElement.dataset.col = squareIndex;
+
+      if (square) {
+        const pieceElement = document.createElement("div");
+        pieceElement.classList.add(
+          "piece",
+          square.color === "w" ? "white" : "black",
+        );
+
+        pieceElement.innerText = "";
+        pieceElement.draggable = playerRole === square.color;
+
+        pieceElement.addEventListener("dragstart" , () => {
+            
+        })
+      }
+    });
+  });
+};
+
+renderBoard();

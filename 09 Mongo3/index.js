5e2feee84ecb2a75938dbba9fe2a05d32a051bc1
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-08-02 12:45:43 +0530

    update route completed

--- 09 Mongo3/index.js
+++ 09 Mongo3/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const ejs = require("ejs");
 const Chat = require("./models/chat.model");
+const methodOverride = require("method-override");
 
 const app = express();
 const PORT = 8000;
@@ -11,6 +12,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 main()
   .then((res) => {
@@ -47,6 +49,27 @@ app.post("/chats", async (req, res) => {
     created_at: new Date(),
   });
   await newChat.save();
+  res.redirect("/chats");
+});
+
+//edit route
+app.get("/chats/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const chat = await Chat.findById(id);
+  res.render("edit.ejs", { chat });
+});
+
+//update Route
+app.patch("/chats/:id", async (req, res) => {
+  const { id } = req.params;
+  const { message } = req.body;
+  const updatedChat = await Chat.findByIdAndUpdate(
+    id,
+    { $set: { message } },
+    { runValidators: true },
+    { new: true },
+  );
+  
   res.redirect("/chats");
 });
 

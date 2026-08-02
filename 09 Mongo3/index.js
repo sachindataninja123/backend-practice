e5fb00713cc1026c40355dc6e651a360e15699e6
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-08-02 11:16:26 +0530

    chat model added with data

--- 09 Mongo3/index.js
+++ 09 Mongo3/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const ejs = require("ejs")
+const ejs = require("ejs");
+const Chat = require("./models/chat.model");
 
 const app = express();
 const PORT = 8000;
@@ -18,6 +19,20 @@ main()
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
+
+let chat1 = new Chat({
+  from: "Neha",
+  to: "Priya",
+  message: "Send me your exam sheets",
+  created_at: new Date(),
+});
+
+chat1
+  .save()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("server is connected!");

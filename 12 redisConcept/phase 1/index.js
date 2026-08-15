9b00364d8eb50babf3260aa325a52926659ff652
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-08-15 10:26:19 +0530

    otp store concept in redis

--- 12 redisConcept/phase 1/index.js
+++ 12 redisConcept/phase 1/index.js
@@ -20,6 +20,8 @@ app.get("/", (req, res) => {
 
 app.post("/create", async (req, res) => {
   const { username, email, phone } = req.body;
+
+  await redis.del("user:all");
   const user = await User.create({
     username,
     email,
@@ -30,22 +32,51 @@ app.post("/create", async (req, res) => {
 });
 
 app.get("/users", async (req, res) => {
-  const user = await User.find();
+  const user = await User.find({});
 
   return res.status(200).json(user);
 });
 
 app.get("/users-with-redis", async (req, res) => {
-  const cached = await redis.get("username:all");
+  const cached = await redis.get("user:all");
 
   if (cached) {
+    const user = JSON.parse(cached);
     return res.status(200).json(user);
   }
 
-  const user = await User.find();
-  await redis.set("username:all", JSON.stringify(user));
+  const user = await User.find({});
+  await redis.set("user:all", JSON.stringify(user));
 
   return res.status(200).json(user);
+});
+
+app.post("/send-otp", async (req, res) => {
+  const { email } = req.body;
+
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  await redis.set(`otp:${email}`, otp, "EX", 60);
+
+  return res.status(201).json({
+    otp: otp,
+  });
+});
+
+app.post("/verify-otp", async (req, res) => {
+  const { email, otp } = req.body;
+
+  const cachedOtp = await redis.get(`otp:${email}`);
+  if (!cachedOtp) {
+    return res.status(400).json({ message: "Otp is expired or not found!" });
+  }
+
+  if (cachedOtp !== otp) {
+    return res.status(400).json({ message: "Otp mismatch error!" });
+  }
+
+  await redis.del(`otp:${email}`);
+
+  return res.status(200).json({ message: "Otp verified!" });
 });
 
 app.listen(PORT, () => {

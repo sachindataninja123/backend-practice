2a127bd43ef9c30db19b0e7323f8724fb33f5de3
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-08-16 22:44:24 +0530

    api gateway

---
+++ 13 scaling System-design/phase 2/backend/services/product/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is started from product service!!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
